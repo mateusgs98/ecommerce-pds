12eddbfc4e2da808f3d1327dc1821e93fb5efd7c
Lucas Viana <lucasgetviana@gmail.com>
2023-05-31 02:41:50 -0300

    fazendo o dashboard

--- front-end/smart-vac/src/helpers/pages/dashboard/style.ts
+++ front-end/smart-vac/src/helpers/pages/dashboard/style.ts
@@ -5,3 +5,21 @@ export const DashboardContainer = styled.div`
     flex-direction: column;
     gap: 80px;
 `
+
+export const CheckboxContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    align-items: center;
+
+    .custom-checkbox {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        align-items: center;
+        input {
+            width: 20px;
+            height: 20px;
+        }
+    }
+`
--- front-end/smart-vac/src/pages/dashboard/index.tsx
+++ front-end/smart-vac/src/pages/dashboard/index.tsx
@@ -1,47 +1,129 @@
+import { api } from "@/Lib/api";
 import { Input } from "@/components/FormElements/Input";
-import { DashboardContainer } from "@/helpers/pages/dashboard/style";
+import { InputContainer } from "@/components/FormElements/Input/style";
+import { CheckboxContainer, DashboardContainer } from "@/helpers/pages/dashboard/style";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 
-interface Vacinas {
-  id: number;
-  Vacina: string;
-  DoencasEvitadas: string;
-  PeriodoEntreDoses: number;
-  Doses: Doses[];
+interface Vacina {
+  vacina: string;
+  doencasEvitadas: string[];
+  periodoEntreDoses: number;
+  doses: Dose[];
 }
 
-interface Doses {
-  NumeroDose: number;
-  Tomou: boolean;
+interface Dose {
+  numeroDose: number;
+  tomou: boolean;
+}
+
+interface TabelaVacinas {
+  vacina: string;
+  doencasEvitadas: JSX.Element;
+  periodoEntreDoses: number;
+  actions: JSX.Element;
 }
 
 const columnsTable = [
-  { dataField: 'Vacina', text: 'Vacina' },
-  { dataField: 'DoencasEvitadas', text: 'Doencas Evitadas' },
-  { dataField: '', text: 'Período Entre Doses (Dias)' },
+  { dataField: 'vacina', text: 'Vacina' },
+  { dataField: 'doencasEvitadas', text: 'Doencas Evitadas' },
+  { dataField: 'periodoEntreDoses', text: 'Período Entre Doses (Dias)' },
   { dataField: 'actions', text: 'Doses' },
 ];
 
 export default function Home() {
   const { data: session } = useSession();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<TabelaVacinas[]>([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   async function fetchData() {
-    const response = await fetch('http://localhost:3000/api/vacinas');
-    const data = await response.json();
-    setData(data.map((register: Vacinas) => ({
+    // const response = await api.get('/esquema-vacinal');
+    // const data = response.data as Vacinas[];
+    const data = [
+      {
+        vacina: 'Vacina 1',
+        doencasEvitadas: [
+          'Doença 1',
+          'Doença 2'
+        ],
+        periodoEntreDoses: 30,
+        doses: [
+          {
+            numeroDose: 1,
+            tomou: true
+          },
+          {
+            numeroDose: 2,
+            tomou: false
+          },
+          {
+            numeroDose: 3,
+            tomou: false
+          }
+        ]
+      },
+      {
+        vacina: 'Vacina 2',
+        doencasEvitadas: [
+          'Doença 1',
+          'Doença 2',
+          'Doença 3'
+        ],
+        periodoEntreDoses: 10,
+        doses: [
+          {
+            numeroDose: 1,
+            tomou: true
+          },
+          {
+            numeroDose: 2,
+            tomou: false
+          },
+          {
+            numeroDose: 3,
+            tomou: false
+          }
+        ]
+      },
+      {
+        vacina: 'Vacina 3',
+        doencasEvitadas: [
+          'Doença 1',
+          'Doença 3',
+        ],
+        periodoEntreDoses: 25,
+        doses: [
+          {
+            numeroDose: 1,
+            tomou: true
+          },
+          {
+            numeroDose: 2,
+            tomou: false
+          },
+          {
+            numeroDose: 3,
+            tomou: true
+          }
+        ]
+      }
+    ] as Vacina[];
+    setData(data.map((register: Vacina) => ({
       ...register,
-      actions: register.Doses.map((dose: Doses) => (
-        <>
-          <Input name={dose.NumeroDose.toString()} label={`${dose.NumeroDose}ª`} type="checkbox" id={dose.NumeroDose} />
-        </>
-      ))
+      doencasEvitadas: (<div dangerouslySetInnerHTML={{ __html: register.doencasEvitadas.join('<br />') }} />),
+      actions: (
+        <CheckboxContainer>
+          {register.doses.map((dose: Dose) => (
+            <div className='custom-checkbox'>
+              <label htmlFor={dose.numeroDose.toString()}>{dose.numeroDose}ª</label>
+              <input type="checkbox" id={dose.numeroDose.toString()} checked={dose.tomou} />
+            </div>))}
+        </CheckboxContainer>
+      )
     })));
   }
 
@@ -54,7 +136,7 @@ export default function Home() {
         </p>
       </div>
       <div>
-        <BootstrapTable keyField='id' data={data} columns={columnsTable} />
+        <BootstrapTable keyField='Vacina' data={data} columns={columnsTable} striped />
       </div>
 
     </DashboardContainer>
